0ee658122b934b16c4d1e72578c1e0aa40518b85
NickRobbins <59617139+NickRobbins@users.noreply.github.com>
2020-01-09 09:41:03 -0700

    Add files via upload
    lbs to kg program

---
+++ 1-9-2020 in class assignment/1-9-2020 in class assignment/1-9-2020 in class assignment.cpp
@@ -0,0 +1,13 @@
+// Nick Robbins
+//
+
+#include <iostream>
+using namespace std;
+int main(){
+    float kilogram, pound;
+    cout << "Enter pound(s): ";
+    cin >> pound;
+    kilogram = pound * .454;
+    cout << pound << " pound = " << kilogram << " kilogram(s): ";
+	return 0;
+}
